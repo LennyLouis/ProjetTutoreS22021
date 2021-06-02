60ac38630d29d328956062d711b89f1434db8e5c
Pablo <pablo.foucher1@gmail.com>
2021-06-02 21:34:49 +0200

    ajout interface

---
+++ src/reconstitution/controller/TeacherMenuController.java
@@ -0,0 +1,19 @@
+package reconstitution.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TeacherMenuController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
