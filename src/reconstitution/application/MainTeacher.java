78c49370d66705183789e5b6eed962179016fe74
Pablo <pablo.foucher1@gmail.com>
2021-06-03 08:47:57 +0200

    ajout mainteacher

--- src/reconstitution/application/MainTeacher.java
+++ src/reconstitution/application/MainTeacher.java
@@ -1,4 +1,28 @@
 package reconstitution.application;
 
-public class MainTeacher {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class MainTeacher extends Application{
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/ressources/view/teacherHomeView.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Reconsitution");
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
