f4a9ca84f750c645e093c8d7b590da5bb0f3091e
lennylouis <lenny.louis35@gmail.com>
2021-06-14 16:22:13 +0200

    Import d'un fichier de rendu d'exercice

--- src/reconstitution/controller/TeacherCorrectController.java
+++ src/reconstitution/controller/TeacherCorrectController.java
@@ -2,14 +2,24 @@ package reconstitution.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.stage.FileChooser;
 import reconstitution.MainTeacher;
+import reconstitution.models.Exercice;
+import reconstitution.models.Resultat;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class TeacherCorrectController implements Initializable {
 
+    private Resultat resultat;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 
     @FXML
     public void homeButton() throws IOException {
@@ -18,11 +28,17 @@ public class TeacherCorrectController implements Initializable {
 
     @FXML
     public void immport() throws IOException {
-
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Importer un rendu d'exercice");
+        File file = fileChooser.showOpenDialog(MainTeacher.getStage());
+        if(file!=null) {
+            try {
+                resultat = (Resultat) Exercice.ouvrir(file.getAbsolutePath());
+            } catch (Exception e){
+                e.printStackTrace();
+                return;
+            }
+            //resultat.get....
+        }
     }
 }
